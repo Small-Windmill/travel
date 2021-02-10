98ec1805aa8501f11d6cc3abcc13b4a98edcb10f
Small-Windmill <62464147+Small-Windmill@users.noreply.github.com>
2021-02-10 13:58:50 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue';
+import fastClick from 'fastclick';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './assets/styles/reset.css';
+import './assets/styles/border.css';
 
 Vue.config.productionTip = false;
-
+fastClick.attach(document.body);
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  // render: (h) => h(App),
+  // }).$mount('#app');
+  components: { App },
+  template: '<App/>',
+});
